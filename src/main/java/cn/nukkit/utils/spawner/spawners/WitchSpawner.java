9539f66a6676910b5b9d55b8b86c2fcbfd49e22b
SocialMoods <nikelev01@gmail.com>
2026-02-02 16:51:53 +1000

    chore: disable witch

--- src/main/java/cn/nukkit/utils/spawner/spawners/WitchSpawner.java
+++ src/main/java/cn/nukkit/utils/spawner/spawners/WitchSpawner.java
@@ -16,15 +16,16 @@ public class WitchSpawner extends AbstractEntitySpawner {
 
     @Override
     public void spawn(Player player, Position pos, Level level) {
-        final int biomeId = level.getBiomeId((int) pos.x, (int) pos.z);
-        if (Utils.rand(1, 5) == 1 && biomeId != 6 && biomeId != 134) {
-            return;
-        }
-        if (level.getBlockLightAt((int) pos.x, (int) pos.y + 1, (int) pos.z) == 0) {
-            if (level.isMobSpawningAllowedByTime()) {
-                this.spawnTask.createEntity("Witch", pos.add(0.5, 1, 0.5));
-            }
-        }
+        // i need it, i know this a shit solution
+//        final int biomeId = level.getBiomeId((int) pos.x, (int) pos.z);
+//        if (Utils.rand(1, 5) == 1 && biomeId != 6 && biomeId != 134) {
+//            return;
+//        }
+//        if (level.getBlockLightAt((int) pos.x, (int) pos.y + 1, (int) pos.z) == 0) {
+//            if (level.isMobSpawningAllowedByTime()) {
+//                this.spawnTask.createEntity("Witch", pos.add(0.5, 1, 0.5));
+//            }
+//        }
     }
 
     @Override
