55e3353c3b518f67a9fb9ce2f4bcc262e04e297f
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2026-08-19 15:39:56 +0300

    feat: EntityFallEvent

---
+++ src/main/java/cn/nukkit/event/entity/EntityFallEvent.java
@@ -0,0 +1,30 @@
+package cn.nukkit.event.entity;
+
+import cn.nukkit.block.Block;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.event.Cancellable;
+import lombok.ToString;
+
+@ToString
+public class EntityFallEvent extends EntityEvent implements Cancellable {
+    private Block blockFallOn;
+    private float fallDistance;
+
+    public EntityFallEvent(Entity entity, Block blockFallOn, float fallDistance) {
+        this.entity = entity;
+        this.blockFallOn = blockFallOn;
+        this.fallDistance = fallDistance;
+    }
+
+    public Block getBlockFallOn() {
+        return blockFallOn;
+    }
+
+    public float getFallDistance() {
+        return fallDistance;
+    }
+
+    public void setFallDistance(float distance) {
+        this.fallDistance = distance;
+    }
+}
